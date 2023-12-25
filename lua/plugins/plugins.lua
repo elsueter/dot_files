412c713d4634c1e2964bfcdc5a8227b2649ab20d
Joseph Straw <joseph_straw@outlook.com>
2023-12-25 11:17:40 +0000

    Updated dashboard

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -187,4 +187,28 @@ return {
 	},
 	{ "tpope/vim-sleuth" },
 	{ "f-person/git-blame.nvim" },
+	{
+		"rcarriga/nvim-notify",
+		keys = {
+			{
+				"<leader>un",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Dismiss all Notifications",
+			},
+		},
+		opts = {
+			timeout = 3000,
+			max_height = function()
+				return math.floor(vim.o.lines * 0.75)
+			end,
+			max_width = function()
+				return math.floor(vim.o.columns * 0.75)
+			end,
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { zindex = 100 })
+			end,
+		},
+	},
 }
