b8e987b75e622a0de4f4c674a3e5d10774c546a7
Joseph Straw <joseph_straw@outlook.com>
2026-02-20 01:55:21 +0000

    Moved scripts into makefile to tidy up

--- factorio/server_startup.sh
+++
@@ -1,2 +0,0 @@
-cd factorio
-./bin/x64/factorio --start-server ../current_world.zip --server-settings ../server-settings.json
--- factorio/update_server.sh
+++
@@ -1,5 +0,0 @@
-wget https://factorio.com/get-download/stable/headless/linux64
-rm -rf factorio
-tar -xf linux64
-rm -rf linux64
-systemctl restart factorio
